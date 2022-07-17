1f86c15127ea9a48ef039ec5b77219aa77faa364
ColombaVellotti <vellotti.colomba1996@gmail.com>
2022-07-17 19:05:44 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './App.module.css'  
 import {Cards, Charts, CountryPicker } from './Components'  
 import {fetchData} from './api';  
-import logo from './images/image.png';  
+import logo from './images/image.png';
 class App extends React.Component {  
     state = {         
     data: {},  
