35214e66e50627d7e7453d253a403706ade296cd
ColombaVellotti <vellotti.colomba1996@gmail.com>
2022-07-21 14:22:15 +0200

    ok

---
+++ src/Components/Header/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Italy",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "United Stated",
+        url: "#",
+        cName: "nav-links"
+    }
+]
