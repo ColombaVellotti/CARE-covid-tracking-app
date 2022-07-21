9dd9a7b7777a4904cf324a5011cfe1d5c6f8ba9d
ColombaVellotti <vellotti.colomba1996@gmail.com>
2022-07-21 14:42:46 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';  
 import styles from './App.module.css'  
-import {Cards, Charts, CountryPicker } from './Components' 
+import { Cards, Charts, CountryPicker } from './Components'
+import { Header } from "./Components" 
 import {fetchData} from './api';  
 import logo from './images/image.png';
 
